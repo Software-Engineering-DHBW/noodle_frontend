7aa41882471d3d2a2a826e9c9d0492468c74e38e
HannesRosenthal <66249431+HanniBal27-2001@users.noreply.github.com>
2022-03-04 21:30:27 +0100

    add input validation and login via enter

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,19 +7,32 @@
       src="/noodle_black.png"
       style="margin-top: 20px; margin-bottom: 20px"
     />
-    <v-form>
+    <v-form
+      ref="loginForm"
+      v-model="valid"
+      lazy-validation
+    >
       <v-text-field
         v-model="user.username"
         label="Username"
         outlined
+        :rules="textFieldRules"
+        @keydown.enter="handleLogin"
       />
       <v-text-field
         v-model="user.password"
-        outlined
         label="Password"
         type="password"
+        autocomplete="on"
+        outlined
+        :rules="textFieldRules"
+        @keydown.enter="handleLogin"
       />
-      <v-btn @click="handleLogin()">
+      <v-btn
+        class="primary"
+        :disabled="!valid"
+        @click="handleLogin()"
+      >
         Login
       </v-btn>
     </v-form>
@@ -34,7 +47,14 @@ const Auth = namespace('Auth');
 
 @Component
 export default class Login extends Vue {
-  private user: any = { username: '', password: '' };
+  private user: any = {
+    username: '',
+    password: '',
+  };
+
+  private valid = true
+
+  private textFieldRules = [(v: any) => !!v || 'Field is required']
 
   @Auth.Getter
   private isLoggedIn!: boolean;
@@ -49,6 +69,10 @@ export default class Login extends Vue {
   }
 
   handleLogin(): void {
+    // validation of name and passwort field
+    const form: any = this.$refs.loginForm;
+    form.validate();
+
     if (this.user.username && this.user.password) {
       this.login(this.user).then(
         () => {
