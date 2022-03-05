8b866e62ada2964eaca8108bbba5d2b6526ac57b
HannesRosenthal <66249431+HanniBal27-2001@users.noreply.github.com>
2022-03-05 18:05:22 +0100

    mobile optimization

--- src/views/student/ModuleOverview.vue
+++ src/views/student/ModuleOverview.vue
@@ -1,35 +1,36 @@
 <template>
   <v-container>
     <v-tabs
-      v-if="$vuetify.breakpoint.xl"
       v-model="activeSemesterTab"
+      center-active
+      show-arrows
     >
       <v-tab
-        v-for="semesterName in semesterNames"
-        :key="semesterName"
+        v-for="semesterNumber in semesterNumbers"
+        :key="semesterNumber"
       >
-        Semester {{ semesterName }}
+        Semester {{ semesterNumber }}
       </v-tab>
     </v-tabs>
 
-    <v-select
-      v-if="$vuetify.breakpoint.xs"
-      v-model="activeSemesterTab"
-      :items="modules"
-      item-text="semester"
-      label="Semester"
-      outlined
-    />
-
-    <v-container
-      v-for="module in semesterModules"
-      :key="module.name"
-      class="module-container text-left"
-    >
-      <h3>{{ module.name }}</h3>
-      <v-divider /><br>
-      {{ module.description }}
-    </v-container>
+    <v-tabs-items v-model="activeSemesterTab">
+      <v-tab-item
+        v-for="semesterNumber in semesterNumbers"
+        :key="semesterNumber"
+      >
+        <v-container
+          v-for="module in semesterModules(semesterNumber)"
+          :key="module.name"
+          class="module-container text-left"
+          @click="moduleClicked(module.name)"
+        >
+          <h3>{{ module.name }}</h3>
+          <v-divider />
+          <br>
+          {{ module.description }}
+        </v-container>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
@@ -65,20 +66,29 @@ export default class ModuleOverview extends Vue {
       description: '9 von 7 Menschen sind mit Mathe überfordert',
     },
     {
-      name: 'Elektronik 3',
-      semester: 3,
+      name: 'Elektronik 5',
+      semester: 5,
       description: 'Halbleiterbauelemente, wo den elektrischen Strom leiten',
     },
-  ]
+    {
+      name: 'Mathematik 5',
+      semester: 5,
+      description: '9 von 7 Menschen sind mit Mathe überfordert',
+    },
+  ];
 
-  private activeSemesterTab: any = 1;
+  private activeSemesterTab: any = null;
 
-  get semesterNames(): Array<number> {
+  get semesterNumbers(): Array<number> {
     return [...new Set(this.modules.map((module) => module.semester))];
   }
 
-  get semesterModules(): Array<any> {
-    return this.modules.filter((module) => module.semester === this.activeSemesterTab);
+  semesterModules(semester: number): Array<any> {
+    return this.modules.filter((module) => module.semester === semester);
+  }
+
+  moduleClicked(module: string): void {
+    this.$router.push(`/module/${module}`);
   }
 }
 </script>
